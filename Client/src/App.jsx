88bea01cd000fd09b96e650a8c09d1afff6d0867
NoNameCoder <johnthetheologian@mail.ru>
2025-08-23 13:48:28 +0300

    Update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Price from "./components/Price";
 
 function App() {
   return (
     <main>
+      <Header/>
+      <Hero/>
+      <Price/>
       <Contact/>
     </main>
   );
---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <ul>
+            <li>
+                <a href="">Услуги</a>
+                
+            </li>
+            <li>
+                <a href="">Контакты</a>
+            </li>
+        </ul>
+    </div>
+  )
+}

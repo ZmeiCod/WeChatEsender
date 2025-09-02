ce23bdadb69be2a7f6b07346e03b6ebeb162b280
NoNameCoder <johnthetheologian@mail.ru>
2025-09-02 19:50:48 +0300

    111

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
+import Preview from "./components/Preview";
 import Price from "./components/Price";
 
 function App() {
   return (
     <main>
       <Header/>
-      <Hero/>
+      <Preview/>
       <Price/>
       <Contact/>
       <Footer/>
--- Client/src/components/Preview.jsx
+++ Client/src/components/Preview.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Hero() {
+export default function Preview() {
   return (
     <div>
       <h1>Регистрация и перепрошивка без забот</h1>
